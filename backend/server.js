15d2665a93c51a6629c88751bea47ed8b61293c5
John Paul Dellova <dellova36@gmail.com>
2024-11-13 03:32:07 +0800

    Firebase Service Account Provider

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
 dotenv.config()
 
+const admin = require('firebase-admin');
+const serviceAccount = require('./serviceAccount.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+})
+
 mongoose.connect(process.env.MONGO_URL).then(() => console.log('Database Connected')).catch((err) => console.log(err))
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(process.env.PORT || port, () => console.log(`HalalExpress app listening on port ${process.env.PORT}!`))
