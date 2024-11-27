de4e78beee425d7b54dbb9a511621f73191c6881
John Paul Dellova <dellova36@gmail.com>
2024-11-27 21:27:12 +0800

    Cart Models

---
+++ backend/models/Cart.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const cartSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, ref: 'User'
+    },
+    productId: {
+        type: mongoose.Schema.Types.ObjectId, ref: 'Food'
+    },
+    additives: {
+        type: [],
+        default: ''
+    },
+    instructions: {
+        type: String,
+        default: ''
+    },
+    quantity: {
+        type: Number,
+        default: 1
+    },
+    totalPrice: {
+        type: Number,
+        required: true
+    },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Cart', cartSchema)
