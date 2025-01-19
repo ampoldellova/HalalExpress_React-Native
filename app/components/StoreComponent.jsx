cb0a0f27c0deb944af9ebc0451a979e024cdfd66
John Paul Dellova <dellova36@gmail.com>
2025-01-19 23:47:47 +0800

    Open and Close Badge adjustment

--- app/components/StoreComponent.jsx
+++ app/components/StoreComponent.jsx
@@ -27,8 +27,8 @@ const StoreComponent = ({ item, onPress }) => {
                 />
                 <Text style={styles.heading}>{item.title}</Text>
                 <View style={{ flex: 1 }} />
-                <View style={{ backgroundColor: item.isAvailable ? 'green' : 'red', borderRadius: 30 }}>
-                    <Text style={[styles.small, { color: 'white', marginHorizontal: 5, marginTop: 3 }]}>{item.isAvailable ? 'Open' : 'Closed'}</Text>
+                <View style={{ backgroundColor: item.isAvailable ? 'green' : 'red', borderRadius: 30, height: 20, justifyContent: 'center' }}>
+                    <Text style={[styles.small, { color: 'white', marginHorizontal: 5 }]}>{item.isAvailable ? 'Open' : 'Closed'}</Text>
                 </View>
             </View>
             <View style={{ flexDirection: "row", justifyContent: 'space-between' }}>
