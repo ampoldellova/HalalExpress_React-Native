029125d55829d1736ad74508fea8fc88aa00ed7e
John Paul Dellova <dellova36@gmail.com>
2025-01-26 15:35:13 +0800

    Searched Supplier List

--- app/screens/Search/SearchedSupplier.jsx
+++ app/screens/Search/SearchedSupplier.jsx
@@ -1,14 +1,62 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { RatingInput } from 'react-native-stock-star-rating'
+import React, { useContext } from 'react'
+import { COLORS, SIZES } from '../../constants/theme'
+import { useNavigation } from '@react-navigation/native'
 
-const SearchedSupplier = () => {
+const SearchedSupplier = ({ item }) => {
+    const navigation = useNavigation();
+
     return (
-        <View>
-            <Text>SearchedSupplier</Text>
-        </View>
+        <TouchableOpacity style={styles.wrapper} onPress={() => { }}>
+            <Image
+                source={{ uri: item.imageUrl.url }}
+                style={{
+                    width: SIZES.width - 35,
+                    height: SIZES.height / 5.8,
+                    borderRadius: 15,
+                    borderWidth: 1,
+                    borderColor: COLORS.gray2
+                }}
+            />
+            <Text style={styles.heading}>{item.title}</Text>
+            <View style={{ flexDirection: "row", justifyContent: 'space-between' }}>
+                <Text style={styles.small}>Delivery under:</Text>
+                <Text style={styles.small}>{item.time}</Text>
+            </View>
+            <View style={{ flexDirection: "row", justifyContent: 'space-between' }}>
+                <RatingInput
+                    rating={item.rating}
+                    size={14}
+                    maxStars={5}
+                    setRating={item.rating}
+                    bordered={false}
+                    color={COLORS.primary}
+                />
+                <Text style={styles.small}>{item.ratingCount} + ratings</Text>
+            </View>
+        </TouchableOpacity>
     )
 }
 
 export default SearchedSupplier
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    wrapper: {
+        borderColor: COLORS.lightWhite,
+        padding: 8,
+        borderRadius: 16,
+        marginHorizontal: 10
+    },
+    heading: {
+        fontSize: 14,
+        fontFamily: "regular",
+        color: COLORS.black,
+        marginTop: 5
+    },
+    small: {
+        fontSize: 12,
+        fontFamily: "regular",
+        color: COLORS.gray
+    }
+})
